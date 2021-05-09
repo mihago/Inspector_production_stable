ef3e1f18ce07ca2ac14d47d40980913324d97e26
Миша Саввин <mihago2004@gmail.com>
2021-05-09 16:28:08 +0300

    somes

--- app/src/main/java/com/example/inspector/MediaFragment.java
+++ app/src/main/java/com/example/inspector/MediaFragment.java
@@ -4,29 +4,60 @@ import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LifecycleOwner;
 
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
+
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MediaFragment extends Fragment {
+    ExecutorService mImageCaptureExecutorService;
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+    ImageCapture imageCapture;
+    Preview preview;
+    Camera camera;
+    CameraSelector cameraSelector;
+    PreviewView previewView;
+    ProcessCameraProvider cameraProvider;
     static final int REQUEST_IMAGE_CAPTURE = 1;
-    private static final int REQUEST_TAKE_PHOTO = 1;
+    private static final int REQUEST_TAKE_PHOTO = 3;
     private static final int RESULT_OK = 1;
     private ImageView imageView;
     private Button addPhoto;
@@ -36,7 +67,7 @@ public class MediaFragment extends Fragment {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -66,60 +97,85 @@ public class MediaFragment extends Fragment {
 
 
         super.onCreate(savedInstanceState);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(getContext());
+        mImageCaptureExecutorService = Executors.newSingleThreadExecutor();
+
 
 
     }
+
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
-            // Фотка сделана, извлекаем миниатюру картинки
-            Bundle extras = data.getExtras();
-            Bitmap thumbnailBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(thumbnailBitmap);
+        /*galleryAddPic();
+        try {
+            wait(3000);
+            setPic(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
+
+
+        Toast.makeText(getContext(),"ggggg",Toast.LENGTH_LONG).show();
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+
+            Toast.makeText(getContext(),"ggggg",Toast.LENGTH_LONG);
         }
+
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View v=inflater.inflate(R.layout.fragment_media, container, false);
+
         imageView = v.findViewById(R.id.imageView);
         addPhoto = v.findViewById(R.id.addPhoto);
-        addPhoto.setOnClickListener(v1 -> {
-            Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            try{
-                startActivityForResult(takePhotoIntent, REQUEST_TAKE_PHOTO);
-            }catch (ActivityNotFoundException e){
-                e.printStackTrace();
+        previewView =v.findViewById(R.id.previewView);
+      imageCapture =
+                new ImageCapture.Builder()
+
+                        .build();
+
+
+        cameraProviderFuture.addListener(() -> {
+            try {
+                cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future.
+                // This should never be reached.
             }
-        });
+        }, ContextCompat.getMainExecutor(getActivity()));
+
+
+
+
+
+
 
 
 
         return v;
     }
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Ensure that there's a camera activity to handle the intent
-        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            // Create the File where the photo should go
-            File photoFile = null;
-            try {
-                photoFile = createImageFile();
-            } catch (IOException ex) {
-                // Error occurred while creating the File
+    void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
+      preview = new Preview.Builder()
+                .build();
 
-            }
-            // Continue only if the File was successfully created
-            if (photoFile != null) {
-                Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
-                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            }
-        }
+     cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+       camera = cameraProvider.bindToLifecycle((LifecycleOwner)getContext(), cameraSelector, imageCapture,preview);
     }
+
+
+
+
 }
